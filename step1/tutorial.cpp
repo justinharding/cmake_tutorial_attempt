f4c4a5b833a4ee1478e98ea56f6858ee4d002f46
Justin Harding <justin.harding@midurth.com>
2022-02-13 12:00:48 +1300

    added library

--- step1/tutorial.cpp
+++ step1/tutorial.cpp
@@ -6,6 +6,10 @@
 
 #include "TutorialConfig.h"
 
+#ifdef USE_MYMATH
+#  include "MathFunctions.h"
+#endif
+
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
@@ -21,7 +25,12 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // calculate square root
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
   const double outputValue = sqrt(inputValue);
+#endif
+
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
